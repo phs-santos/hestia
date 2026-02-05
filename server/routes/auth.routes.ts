32f0cc04a58e062169a1bcb1c5c828578f6da5f0
phs-santos <paulo18santos2000@gmail.com>
2026-02-05 04:25:17 -0300

    logout que tinha esquecido

--- server/routes/auth.routes.ts
+++ server/routes/auth.routes.ts
@@ -7,6 +7,7 @@ const router = express.Router();
 router.post('/auth/register', authController.register);
 router.post('/auth/login/email', authController.loginEmail);
 router.post('/auth/login/nickname', authController.loginNickname);
+router.post('/auth/sign-out', authController.signOut);
 router.get('/auth/me', authMiddleware, authController.getMe);
 
 export default router;
